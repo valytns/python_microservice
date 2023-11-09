60c866a588788e62d1e7d24b1d3297ac8b4bb473
Valentin Tanasi <valytns@gmail.com>
2023-11-09 19:51:13 +0200

    fix typos

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
+"""Module docstring"""
 from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def read_root():
+    """function docstring"""
     return {"message": "Hell on World"}
+
 
 @app.get("/data/{name}")
 async def read_data(name: str):
+    """function docstring"""
     return {"message": f"Hell on World {name}"}
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,17 @@
+"""doc string"""
 from fastapi.testclient import TestClient
 from app.main import app
 
 client = TestClient(app)
 
-def test_read_root():
+def test_read_root(): 
+    """doc string"""
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message":"Hell on World"}
 
 def test_read_data():
+    """doc string"""
     response = client.get("/data/ion")
     assert response.status_code == 200
     assert response.json() == {"message":"Hell on World ion"}
